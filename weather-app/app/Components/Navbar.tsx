923c7b9bafa49bca036e6a810d15576869d0b83d
Cheril Yogi <114796225+CY2404@users.noreply.github.com>
2024-04-17 23:32:06 -0400

    .

--- weather-app/app/Components/Navbar.tsx
+++ weather-app/app/Components/Navbar.tsx
@@ -2,7 +2,9 @@
 import React from "react";
 
 function Navbar(){
-    return <div>Navbar</div>;
+    return <div className="w-full py-4 flex items-center justify-between ">
+        <div className="left"></div>
+    </div>;
 }
 
 export default Navbar;
