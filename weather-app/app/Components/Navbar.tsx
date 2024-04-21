c49babdddfd7abd4141431b9239d8f7790ffdc1e
Cheril Yogi <114796225+CY2404@users.noreply.github.com>
2024-04-21 01:01:38 -0400

    .

--- weather-app/app/Components/Navbar.tsx
+++ weather-app/app/Components/Navbar.tsx
@@ -1,10 +1,16 @@
 "use Client"; 
+import { Button } from "@/components/ui/button";
+import { useRouter } from "next/router";
 import React from "react";
 
 function Navbar(){
-    return <div className="w-full py-4 flex items-center justify-between ">
+    const router = useRouter();
+    return (<div className="w-full py-4 flex items-center justify-between ">
         <div className="left"></div>
-    </div>;
+        <div className="search-conatiner flex shrink-0 w-full gap-2 sm:w-fit">
+            <Button className="source-code flex items-center gap-2 ">Search</Button>
+        </div>
+    </div>);
 }
 
 export default Navbar;
